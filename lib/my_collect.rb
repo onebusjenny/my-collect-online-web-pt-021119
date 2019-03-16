e94e879b617c218929a481b985069417cc308549
Jenny Chu <jchudesign0930@gmail.com>
2019-03-16 01:15:39 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+require 'pry'
+
+def my_collect(collection)
+  x = 0 
+  array = []
+  while x < collection.length
+    array << yield(collection[x])
+    x += 1 
+    
+  end
+  array
+end 
+
+
+
 
 
